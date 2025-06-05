c8f33634e6462867895394b4699aea01b28a31fc
Rohan Singh <rohan.singh.en@gmail.com>
2025-06-06 01:20:14 +0530

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import { investedProperties } from "./data/investedProperties.js"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env"
+})
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(cors())
+
+app.get("/api/v1/properties", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: investedProperties
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
